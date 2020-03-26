61211483f8907f6c3adf9dc2f9568b05f840e142
Mandeep Kumar <kumar.26.mandeep@gmail.com>
2020-03-26 16:22:47 -0700

    styles

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -5,65 +5,65 @@ import { Link } from "react-router-dom";
 
 function RenderCampsite({ campsite }) {
 
-      return (
-    <div className="col-md-5 m-1">
-      <Card>
-        <CardImg top src={campsite.image} alt={campsite.name} />
-        <CardBody>
+    return (
+        <div className="col-md-5 m-1">
+            <Card>
+                <CardImg top src={campsite.image} alt={campsite.name} />
+                <CardBody>
 
-          <CardText>{campsite.description}</CardText>
-        </CardBody>
-      </Card>
-    </div>
-  );
+                    <CardText>{campsite.description}</CardText>
+                </CardBody>
+            </Card>
+        </div>
+    );
 }
 function RenderComments({ comments }) {
 
-  if (comments) {
-    return (
-      <div className="col-md-5 m-1">
-        <h4>Comments</h4>
-        {comments.map(comment => (
-          <div key={comment.id}>
-            {" "}
-            {comment.text} <br /> {comment.author} <br />{" "}
-            {new Intl.DateTimeFormat("en-US", {
-              year: "numeric",
-              month: "short",
-              day: "2-digit"
-            }).format(new Date(Date.parse(comment.date)))}
-          </div>
-        ))}
-      </div>
-    );
-  }
+    if (comments) {
+        return (
+            <div className="col-md-5 m-1">
+                <h4>Comments</h4>
+                {comments.map(comment => (
+                    <div key={comment.id}>
+                        {" "}
+                        {comment.text} <br /> {comment.author} <br />{" "}
+                        {new Intl.DateTimeFormat("en-US", {
+                            year: "numeric",
+                            month: "short",
+                            day: "2-digit"
+                        }).format(new Date(Date.parse(comment.date)))}
+                    </div>
+                ))}
+            </div>
+        );
+    }
 
-  return <div />;
+    return <div />;
 
 }
 
 function CampsiteInfo(props) {
-  if (props.campsite) {
-    return (
-      <div className="container">
-        <div className="row">
-          <div className="col">
-            <Breadcrumb>
-              <BreadcrumbItem><Link to="/directory">Directory</Link></BreadcrumbItem>
-              <BreadcrumbItem active>{props.campsite.name}</BreadcrumbItem>
-            </Breadcrumb>
-            <h2>{props.campsite.name}</h2>
-            <hr />
-          </div>
-        </div>
-        <div className="row">
-          <RenderCampsite campsite={props.campsite} />
-          <RenderComments comments={props.comments} />
-        </div>
-      </div>
-    );
-  }
-  return <div />;
+    if (props.campsite) {
+        return (
+            <div className="container">
+                <div className="row">
+                    <div className="col">
+                        <Breadcrumb>
+                            <BreadcrumbItem><Link to="/directory">Directory</Link></BreadcrumbItem>
+                            <BreadcrumbItem active>{props.campsite.name}</BreadcrumbItem>
+                        </Breadcrumb>
+                        <h2>{props.campsite.name}</h2>
+                        <hr />
+                    </div>
+                </div>
+                <div className="row">
+                    <RenderCampsite campsite={props.campsite} />
+                    <RenderComments comments={props.comments} />
+                </div>
+            </div>
+        );
+    }
+    return <div />;
 }
 
 export default CampsiteInfo;
