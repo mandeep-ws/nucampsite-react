afc2bb9030b29d2f0c3d628e8061f084766b8f64
Mandeep Kumar <kumar.26.mandeep@gmail.com>
2020-03-26 12:21:01 -0700

    week3

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -2,32 +2,34 @@ import React, { Component } from "react";
 import {
   Card,
   CardImg,
-  CardImgOverlay,
   CardText,
   CardBody,
-  CardTitle
+  CardTitle,
+  Breadcrumb,
+  BreadcrumbItem
 } from "reactstrap";
+import { Link } from "react-router-dom";
 
-class Campsiteinfo extends React.Component{
+
 
   
 
     
-    renderCampsite(campsite){
+  function  RenderCampsite({campsite}){
     
       return (
                 <div className="col-md-5 m-1">
                     <Card>
                         <CardImg top src={campsite.image} alt={campsite.name} />
                         <CardBody>
-                            <CardTitle>{campsite.name}</CardTitle>
+                            
                             <CardText>{campsite.description}</CardText>
                         </CardBody>
                     </Card>
                 </div>
         );
       }        
-    renderComments(comments){
+  function  RenderComments({comments}){
 
         if(comments){
             return (
@@ -52,22 +54,28 @@ class Campsiteinfo extends React.Component{
 
     }  
     
-    render() {
-
-           if (this.props.campsite) {
-            return(
+    function CampsiteInfo(props) {
+      if (props.campsite) {
+         return (
+            <div className="container">
                 <div className="row">
-                    
-                    {this.renderCampsite(this.props.campsite)}
-                    {this.renderComments(this.props.campsite.comments)}
-                
-                </div>);
-            }
-           
-                return <div />;
-    
-        
-       }
+                    <div className="col">
+                        <Breadcrumb>
+                            <BreadcrumbItem><Link to="/directory">Directory</Link></BreadcrumbItem>
+                            <BreadcrumbItem active>{props.campsite.name}</BreadcrumbItem>
+                        </Breadcrumb>
+                        <h2>{props.campsite.name}</h2>
+                        <hr />
+                    </div>
+                </div>
+                <div className="row">
+                    <RenderCampsite campsite={props.campsite} />
+                    <RenderComments comments={props.comments} />
+                </div>
+            </div>
+        );
+      }
+      return <div />;
     }
 
-export default Campsiteinfo;
+    export default CampsiteInfo;
--- src/components/DirectoryComponent.js
+++ src/components/DirectoryComponent.js
@@ -3,52 +3,58 @@ import {
   Card,
   CardImg,
   CardImgOverlay,
-  CardText,
-  CardBody,
-  CardTitle
+  CardTitle,
+  Breadcrumb,
+  BreadcrumbItem
 } from "reactstrap";
-import Campsiteinfo from "./CampsiteInfoComponent";
+import { Link } from "react-router-dom";
+
+import Main from "./MainComponent"
+
+function RenderDirectoryItem({ campsite }) {
+  return (
+    <Card>
+            <Link to={`/directory/${campsite.id}`}>
+                <CardImg width="100%" src={campsite.image} alt={campsite.name} />
+                <CardImgOverlay>
+                    <CardTitle>{campsite.name}</CardTitle>
+                </CardImgOverlay>
+            </Link>
+        </Card>
+  );
+}
 
 
-class Directory extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      selectedCampsite: null
-    };
-  }
 
-  onCampsiteSelect(campsite) {
-    this.setState({ selectedCampsite: campsite });
-  }
+function Directory(props) {
 
- 
-
-  render() {
-    const directory = this.props.campsites.map(campsite => {
-      return (
-        <div key={campsite.id} className="col-md-5 m-1">
-          <Card onClick={() => this.onCampsiteSelect(campsite)}>
-            <CardImg width="100%" src={campsite.image} alt={campsite.name} />
-            <CardImgOverlay>
-              <CardTitle>{campsite.name}</CardTitle>
-            </CardImgOverlay>
-          </Card>
-        </div>
-      );
+    const directory = props.campsites.map(campsite => {
+        return (
+            <div key={campsite.id} className="col-md-5 m-1">
+                <RenderDirectoryItem campsite={campsite}  />
+            </div>
+        );
     });
 
-    return (
-     
-      <div className="container">
-        <div className="row">{directory}</div>
-        
-
-        <Campsiteinfo campsite={this.state.selectedCampsite} />
     
-      </div>
+     return (
+        <div className="container">
+            <div className="row">
+                <div className="col">
+                    <Breadcrumb>
+                        <BreadcrumbItem><Link to="/home">Home</Link></BreadcrumbItem>
+                        <BreadcrumbItem active>Directory</BreadcrumbItem>
+                    </Breadcrumb>
+                    <h2>Directory</h2>
+                    <hr />
+                </div>
+            </div>
+            <div className="row">
+                {directory}
+            </div>
+        </div>
     );
   }
-}
+
 
 export default Directory;
